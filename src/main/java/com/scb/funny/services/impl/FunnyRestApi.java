71890c66943c180b070be01b91f8b82607f9bbda
abenee <a.benecs@gmail.com>
2020-05-01 17:18:12 +0700

    add funny GET Method

--- src/main/java/com/scb/funny/services/impl/FunnyRestApi.java
+++ src/main/java/com/scb/funny/services/impl/FunnyRestApi.java
@@ -1,9 +1,12 @@
 package com.scb.funny.services.impl;
 
+import javax.validation.Valid;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,12 +30,12 @@ public class FunnyRestApi {
 		produces="application/json",
 		consumes="application/json")
 	@ResponseBody
-	public FunnyResponse funny(@RequestBody FunnyRequest req) throws ServiceException {
+	public FunnyResponse funnyPost(@RequestBody FunnyRequest req) throws ServiceException {
 		log.info("Request MSG:"+new Gson().toJson(req));
 		String funnyStr = req.getFunnyStr();
 		FunnyResponse resp = new FunnyResponse();
 		try {
-			if (funnyStr!=null && funnyStr.length()>0) {
+			if (funnyStr!=null && funnyStr!="") {
 				 funnyStr = Base64Util.decode(funnyStr); //original input
 				 log.info("Original :"+ funnyStr);
 				 
@@ -50,6 +53,46 @@ public class FunnyRestApi {
 				 funnyStr = Base64Util.encode(funnyStr); //encode response
 				 				
 				 resp.setFunnyStr(funnyStr);
+			} else {
+				throw new ServiceException("Input can not be null");
+			}
+		} catch (ServiceException ex){
+			throw new ServiceException(ex.getMessage());
+		} 
+		
+		log.info("Response MSG:"+new Gson().toJson(resp));
+		return resp;
+	} 
+	
+	@RequestMapping(
+		value="/funny/",
+		method=RequestMethod.GET)
+	@ResponseBody
+	public FunnyResponse funnyGet(@RequestParam String funnyStr) throws ServiceException {
+		log.info("Request MSG:"+ funnyStr);
+		
+		FunnyResponse resp = new FunnyResponse();
+		try {
+			if (funnyStr!=null && funnyStr!="") {
+				 funnyStr = Base64Util.decode(funnyStr); //original input
+				 log.info("Original :"+ funnyStr);
+				 
+				 if (!AlphabetUtil.isAlphaBet(funnyStr)) { //check input is Alphabet
+					 throw new ServiceException("It's not AlphaBet");
+				 }
+				 
+				 funnyStr = funnyStr.toLowerCase();	//make as lower case
+				 log.info("lower case :"+ funnyStr);
+				 
+				 funnyStr = ReplaceSpaceUtil.asNumber(funnyStr); //replace white space as numeric
+				 
+				 funnyStr = StringUtils.reverse(funnyStr); //reverse string
+				 
+				 funnyStr = Base64Util.encode(funnyStr); //encode response
+				 				
+				 resp.setFunnyStr(funnyStr);
+			} else {
+				throw new ServiceException("Input can not be null");
 			}
 		} catch (ServiceException ex){
 			throw new ServiceException(ex.getMessage());
